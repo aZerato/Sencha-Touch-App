9f5b286efb3681f77181a39c2a511e9f16599690
Jean-Phi <azerato.graphe@gmail.com>
2014-03-10 17:31:03 +0100

    Update Title Navigation when Edit

--- app/controller/contacts/Edit.js
+++ app/controller/contacts/Edit.js
@@ -1,18 +1,25 @@
 Ext.define('sencha-touch-app.controller.contacts.Edit', {
     extend: 'Ext.app.Controller',
     require: [
+        'sencha-touch-app.view.contacts.Navigation',
     	'sencha-touch-app.view.contacts.Edit',
+        'sencha-touch-app.view.contacts.Detail',
     	'sencha-touch-app.store.Contacts'
     ],
 
     config: {
     	refs: {
-    		contactEdit: 'contactEdit'
+            contactsNavigation: 'contactsNavigation',
+    		contactEdit: 'contactEdit',
+            contactDetail: 'contactDetail'
     	},
     	control: {
     		'contactEdit #submitEditContact': {
     			tap: 'submitContact'
-    		}
+    		},
+            contactsNavigation: {
+                back: 'backNavigation'
+            }
     	}
     },
 
@@ -32,5 +39,20 @@ Ext.define('sencha-touch-app.controller.contacts.Edit', {
 
 		// Update
 		storedContact.set(data);
+
+        var contactsNavigationBar = self.getContactsNavigation().getNavigationBar();
+        contactsNavigationBar.setTitle(data.firstName);
+    },
+
+    backNavigation: function() {
+        var self = this;
+
+        var contactEdit = self.getContactEdit();
+
+        if(contactEdit)
+        {
+           var contactsNavigationBar = self.getContactsNavigation().getNavigationBar();
+           contactsNavigationBar.setTitle(contactEdit.getValues().firstName);
+        }
     }
 });
